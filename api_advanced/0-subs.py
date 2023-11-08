b4d7925c3801fe0f26fad9a2bd5e392aa14cb886
Eliane-M <munezeroeliane761@gmail.com>
2023-11-08 01:55:16 -0800

    api advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,23 +7,32 @@ of a subrredit
 
 
 def number_of_subscribers(subreddit):
+    """
+    returns the number
+    of subscribers
+    of a given subrredit
+    """
+
     import json
     import requests
     import sys
 
     subreddit = sys.argv[1]
-    URL = f"https://www.reddit.com/r/subreddit/about.json"
+    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     headers = {
             "User-Agent": "0-subs/1.0"
     }
 
     raw_response = requests.get(URL, headers=headers)
-    json_response = raw_response.json()
 
-    sub_count = json_response['data']['subscribers']
+    if (raw_response.status_code) == 200:
+        json_response = raw_response.json()
+        sub_count = json_response['data']['subscribers']
+        print (sub_count)
 
-    print (sub_count)
+    else:
+        return 0
 
 if __name__ == "__main__":
     pass
