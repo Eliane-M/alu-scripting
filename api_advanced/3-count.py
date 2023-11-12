ff67393b9a43c18667f5d45f63ff4a7683bdd19f
Eliane-M <munezeroeliane761@gmail.com>
2023-11-12 11:52:42 -0800

    api advanced

---
+++ api_advanced/3-count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+recursive funtion that
+parses the titles of all hot posts
+and prints out a sorted count of given words
+"""
+
+
+import json
+import requests
+import sys
+
+
+def count_words(subreddit, word_list, after=None, results=None):
+    """
+    recursive function
+    """
+    if results is None:
+        results = {}
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 10, 'after': after} if after else {'limit': 10}
+
+    response = requests.get(url, params=params, headers={'User-Agent': 'YOUR_USER_AGENT'})
+
+    if response.status_code != 200:
+        print(f"Error: HTTP Status Code {response.status_code}")
+        return
+
+    data = response.json()
+
+    if 'data' in data and 'children' in data['data']:
+        for child in data['data']['children']:
+            title_words = child['data']['title'].lower().split()
+
+            for word in word_list:
+                if word.lower() in title_words:
+                    results[word.lower()] = results.get(word.lower(), 0) + title_words.count(word.lower())
+
+        count_words(subreddit, word_list, after=data['data'].get('after'), results=results)
+
+       
+    else:
+        print("Error: Unexpected response format")
+
+                                                                                                                                                                                if results:
+        sorted_results = sorted(results.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_results:
+            print(f"{word}: {count}")
+
+
+if __name__ == '__main__':
+    pass
